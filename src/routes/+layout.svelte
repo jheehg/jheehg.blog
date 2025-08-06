1e3086a02ec0cc4e957ee799be3e6372ceb64d59
jheehg <juhee.hg@gmail.com>
2025-08-06 18:03:31 +0900

    style: add styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+	import ScrollUpButton from '$lib/components/ScrollUpButton.svelte';
+	import SideBar from '$lib/components/SideBar.svelte';
+	import ThemeToggleButton from '$lib/components/ThemeToggleButton.svelte';
+	import '$lib/styles/layout.scss';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
 
+<SideBar />
+<ThemeToggleButton />
 {@render children()}
+<ScrollUpButton />
